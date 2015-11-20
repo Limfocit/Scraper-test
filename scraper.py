10e29ed285da51aa93f05f37990d7e1274655b2c
Limfocit <valentin.michajlenko@gmail.com>
2015-11-20 16:48:49 +0300

    added ini for mongo connection, save product to db and process product page

--- scraper.py
+++ scraper.py
@@ -2,6 +2,26 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import time
+import logging
+import hashlib
+from pymongo import MongoClient
+from ConfigParser import SafeConfigParser
+
+
+def save_product_to_mongo(product_dict):
+    mongo_db.product.insert(product_dict)
+
+
+def process_product_page(driver, product_url):
+    product_dict = {}
+    driver.get(product_url)
+    if len(driver.find_elements_by_class_name("notFoundCopy")) > 0:
+        logger.debug("not found %s" % product_url)
+        return None
+    name = driver.find_element_by_class_name("pdpProductTitle").text
+    product_dict["_id"] = hashlib.sha1(name).hexdigest()
+    product_dict["title"] = name
+    return product_dict
 
 
 def scroll_window_and_load_elements(driver, product_xpath):
@@ -31,7 +51,7 @@ def extract_subcategories_urls(driver):
     for top_category in ["Womens-Clothing", "Mens-Clothing", "Girls-1H-12yrs-Clothing",
                       "Girls-9-16yrs-Clothing", "Boys-1H-12yrs-Clothing",
                       "Boys-9-16yrs-Clothing", "Baby-0-3yrs-Clothing"]:
-        driver.get("http://www.boden.co.uk/en-GB/%s" % top_category)
+        driver.get("http://www.boden.co.uk/en-US/%s" % top_category)
         links_to_subcategories += [(link.get_attribute('innerHTML'), link.get_attribute("href"))
                 for link in driver.find_element_by_class_name("MainCategoryMenu").find_elements_by_tag_name("a")
                 if "new in" not in link.get_attribute('innerHTML').lower()]
@@ -39,7 +59,7 @@ def extract_subcategories_urls(driver):
 
 
 def extract_christmas_gifts_urls(driver):
-    driver.get("http://www.boden.co.uk/en-GB/Christmas")
+    driver.get("http://www.boden.co.uk/en-US/Christmas")
     links_to_subcategories = driver.find_elements_by_xpath("//div[@id=\"dropdownChristmas\"]//ul/li/a")
     return [(link.get_attribute('innerHTML'), link.get_attribute("href"))
             for link in links_to_subcategories if "gift vouchers" not in link.get_attribute('innerHTML').lower()]
@@ -48,9 +68,23 @@ def extract_christmas_gifts_urls(driver):
 def scraper_process():
     driver = webdriver.Chrome(executable_path="./chromedriver")
     sections_urls = extract_subcategories_urls(driver) + extract_christmas_gifts_urls(driver)
-    extract_product_links_from_sections(driver, sections_urls)
+    product_urls = extract_product_links_from_sections(driver, sections_urls)
+    for product_url in product_urls:
+        product_dict = process_product_page(driver, product_url)
+        save_product_to_mongo(product_dict)
     # driver.close()
 
 
 if __name__ == "__main__":
-    scraper_process()
+    logger = logging.getLogger()
+
+    config = SafeConfigParser()
+    config.read('local.ini')
+    mongo_client = MongoClient(host=config.get("mongo", "host"), port=int(config.get("mongo", "port")))
+    mongo_db = mongo_client.boden
+    # scraper_process()
+
+    # test code
+    product = process_product_page(webdriver.Chrome(executable_path="./chromedriver"),
+        "http://www.boden.co.uk/en-GB/Girls-1H-12yrs-Dresses/Special-Occasion-Dresses/33394-GLD/Girls-1H-12yrs-Sparkly-Gold-Sparkly-Jersey-Party-Dress.html")
+    save_product_to_mongo(product)
